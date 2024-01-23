6b79c5058184eb34c87a81221f58169cf1f6c80e
jcode2023 <mr.mohammad.pg19@gmail.com>
2024-01-23 14:06:44 +0330

    Start project amiri clothing store

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import type { AppProps } from "next/app"
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+  <>
+  <Head>
+  <link rel="icon" type="image/png" href="https://seeklogo.com/images/A/amiri-logo-0A19AA90E1-seeklogo.com.png"/>
+  <meta name="theme-color" content="#ffffff"/>
+  </Head>
+  <Component {...pageProps} />
+  </>  
+  )
 }
