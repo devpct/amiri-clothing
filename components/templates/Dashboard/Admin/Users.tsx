45fc3f8345f1818edb796e0c26b488765a600bfd
Mohammad Abdollahzadeh <102470936+devpct@users.noreply.github.com>
2024-02-29 15:17:14 +0330

    Update Users.tsx

--- components/templates/Dashboard/Admin/Users.tsx
+++ components/templates/Dashboard/Admin/Users.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Avatar, IconButton } from '@mui/material';
 import { startCase } from 'lodash';
+import axios from 'axios';
+import { mutate } from 'swr';
 
 export default function Users({ usersData }) {
   const [selectedUsers, setSelectedUsers] = useState([]);
@@ -57,6 +59,14 @@ export default function Users({ usersData }) {
     return count;
   };
 
+  const deleteUsers  = async (userIds) => {
+
+    await axios.delete(`http://localhost:4000/users/${userIds}`);
+  
+    mutate('Users');
+  };
+
+  
   return (
     <>
       <section className="container mx-auto">
@@ -111,27 +121,33 @@ export default function Users({ usersData }) {
           </div>
 
           <div className="flex items-center gap-x-6">
-            <button className="text-gray-500 transition-colors duration-200 dark:hover:text-red-500 dark:text-gray-300 hover:text-red-500 focus:outline-none border rounded-full p-2">
-              <svg width="30" height="30" fill="none" stroke="#ff0000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path d="M3 6h18"></path>
-                <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
-                <path d="M10 11v6"></path>
-                <path d="M14 11v6"></path>
-              </svg>
-            </button>
-            <button className="text-gray-500 transition-colors duration-200 dark:hover:text-yellow-500 dark:text-gray-300 hover:text-yellow-500 focus:outline-none border rounded-full p-2">
-              <svg width="30" height="30" fill="none" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <button 
+            className="flex items-center gap-x-2 text-gray-500 transition-colors duration-200 dark:text-gray-300  focus:outline-none border rounded-[10px] p-2 pr-4"
+            onClick={() => deleteUsers(selectedUsers)}
+          >
+            <svg width="25" height="25" fill="none" stroke="#ff0000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path d="M3 6h18"></path>
+              <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+              <path d="M10 11v6"></path>
+              <path d="M14 11v6"></path>
+            </svg>
+            <p className='text-red-500'>Delete</p>
+          </button>
+            <button className="flex items-center gap-x-2 text-gray-500 transition-colors duration-200 dark:text-gray-300  focus:outline-none border rounded-[10px] p-2 pr-4">
+              <svg width="25" height="25" fill="none" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
                 <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
               </svg>
+              <p>Edit</p>
             </button>
-            <button className="flex items-center gap-x-2 text-gray-500 transition-colors duration-200 dark:hover:text-yellow-500 dark:text-gray-300 hover:text-yellow-500 focus:outline-none border rounded-full p-2 pr-4">
-            <svg width="30" height="30" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <button className="flex items-center gap-x-2 text-gray-500 transition-colors duration-200 dark:text-gray-300  focus:outline-none border rounded-[10px] p-2 pr-4">
+            <svg width="25" height="25" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path d="M12 5v14"></path>
               <path d="M5 12h14"></path>
             </svg>
             <p>Add</p>
             </button>
+
           </div>
         </div>
 
